4d93da506ba87f5fdfc62ab0fdb765dc082e04f8
KiwonKim <84374354+kionys@users.noreply.github.com>
2024-09-05 17:31:00 +0900

    [Bronze V] Title: A+B - 5, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -0,0 +1,11 @@
+
+def calc():
+    while True:
+        A, B = map(int, input().split())
+        if A == 0 or B == 0: 
+            break
+        else:
+            print(A + B)
+
+        
+calc()
