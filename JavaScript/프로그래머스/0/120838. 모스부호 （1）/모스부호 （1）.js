eb4dcc79662d0ce0cb8cae39fb967fc0058c7092
KiwonKim <kiwon7271@naver.com>
2024-09-30 21:07:16 +0900

    [level 0] Title: 모스부호 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,36 @@
+const morse = {
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+};
+function solution(letter) {
+  let letterSplits = letter.split(" ");
+  const parseLetter = letterSplits.map(letter => {
+    return morse[letter];
+  });
+
+  return Object.values(parseLetter).join("");
+}
