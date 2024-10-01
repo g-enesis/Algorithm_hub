f5227cd6bee0037361c05beab0db8e894e0fb88f
KiwonKim <kiwon7271@naver.com>
2024-10-01 13:12:24 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ JavaScript/프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -2,16 +2,14 @@ function solution(my_str, n) {
   const answer = [];
   let s = 0;
 
-  // 반복 횟수 계산 (전체 문자열 길이를 n으로 나누어 올림)
+  // 필요한 반복 횟수 계산 (소수점 올림)
   const forIdx = Math.ceil(my_str.length / n);
-  
+
   for (let i = 0; i < forIdx; i++) {
-    // 문자열을 n 길이로 잘라서 answer 배열에 추가
-    let slicedStr = my_str.slice(s, s + n);
+    let slicedStr = my_str.slice(s, s + n); // s에서 시작하여 n만큼 슬라이스
     answer.push(slicedStr);
     s += n; // 시작 인덱스를 n만큼 증가시킴
   }
 
   return answer;
 }
-
