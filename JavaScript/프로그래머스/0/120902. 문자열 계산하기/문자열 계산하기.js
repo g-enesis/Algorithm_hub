acdd0c7e76dd90c87a0fde9c95550ea3cfd40857
KiwonKim <kiwon7271@naver.com>
2024-10-01 20:38:15 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ JavaScript/프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -2,7 +2,6 @@ function solution(my_string) {
   var answer = my_string.split(" ");
   let value = parseInt(answer[0]);
 
-  // console.log("fist value: ", value);
   let numItems = [];
   let calcItems = [];
 
@@ -11,21 +10,14 @@ function solution(my_string) {
     if (item === "+" || item === "-") calcItems.push(item);
   });
 
-  // console.log("numItems: ", numItems);
-  // console.log("calcItems:", calcItems);
-
   calcItems.map((item, i) => {
-    // console.log(item);
     if (item === "+") {
       value += parseInt(numItems[i + 1]);
-      // console.log(value);
     }
     if (item === "-") {
       value -= parseInt(numItems[i + 1]);
-      // console.log(value);
     }
   });
-  // console.log("last value: ", value);
   return value;
 }
 solution("3 + 4 - 1 + 1");
