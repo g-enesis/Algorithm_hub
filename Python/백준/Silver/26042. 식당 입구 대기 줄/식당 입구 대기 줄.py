1a21a158927b3f1dc078a25c2a0d15d965896450
KiwonKim <kiwon7271@naver.com>
2024-10-01 09:32:49 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 308 ms, Memory: 34972 KB -BaekjoonHub

--- Python/백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
+++ Python/백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
@@ -1,20 +1,23 @@
-input = __import__('sys').stdin.readline
+import sys
+
+input = sys.stdin.readline
 
 n = int(input())
-stack = []	#현재 대기줄
-maxs = [0,0]
+stack = []	
+max_idx = [0,0]
 for _ in range(n):
-    info = list(map(int,input().split()))	#입력
-    if len(info) == 1:	#입력이 2라면?
-        stack.pop(0)	#식사가 준비되었으니 현재 대기줄 맨 앞에 있는 인원을 제거해준다
-    else:	#아니라면 마지막에 줄을 세운다
+    info = list(map(int,input().split()))	
+    if len(info) == 1:
+        stack.pop(0)
+    else:
         stack.append(info[1])
 
-    if maxs[0] < len(stack):	#현재 줄에 최대 인원이 있다면 갱신해준다
-        maxs[0] = len(stack)
-        maxs[1] = stack[-1]
-    elif maxs[0] == len(stack):	#현재 줄에 인원이 최대라면? 마지막 인원이 작은 값을 취해준다
-        maxs[1] = min(maxs[1],stack[-1])
+    student_idx = len(stack)
+    if max_idx[0] < student_idx:
+        max_idx[0] = student_idx
+        max_idx[1] = stack[-1]
+    elif max_idx[0] == student_idx:
+        max_idx[1] = min(max_idx[1],stack[-1])
 
-print(maxs[0],maxs[1])
+print(max_idx[0],max_idx[1])
  
