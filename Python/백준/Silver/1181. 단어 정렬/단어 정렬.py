d08987c3eea2be03e3391807c9e52c4d8481732d
Kiwon Kim <kiwon7271@naver.com>
2024-10-04 16:46:30 +0900

    [Silver V] Title: 단어 정렬, Time: 4140 ms, Memory: 34508 KB -BaekjoonHub

--- Python/백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ Python/백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -15,7 +15,22 @@ for _ in range(N):
        word.append(w)
 
 # 길이가 짧은 순으로 정렬하고, 길이가 같으면 사전순으로 정렬
-sorted_word = sorted(word, key=lambda x: (len(x), x))
+sorted_word = sorted(word, key=lambda w: (len(w), w))
+# lambda x: (len(x), x)는 x라는 단어에 대해 그 단어의 (길이, 단어)를 반환하는 함수입니다.
+# sorted() 함수는 이 반환값을 기준으로 먼저 길이를 기준으로 정렬하고, 길이가 같으면 단어 자체를 사전 순으로 정렬합니다.
 
 for sw in sorted_word:
     print(sw)
+
+# lambda
+"""
+def add(a, b):
+    return a + b
+
+lambda 
+> add = lambda a, b: a + b
+
+lambda arguments: expression
+- arguments : 함수에 전달되는 매개변수
+- expression : 리턴되는 값(단일 표현식)
+"""
